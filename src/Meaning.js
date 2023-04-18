df0935a747b0c59486155773c03305c3f4af1d81
MarydeR <122215523+MarydeR@users.noreply.github.com>
2023-04-18 12:18:31 +0200

    update on definition render

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,8 +5,8 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <span>{props.data.partOfSpeech}</span>
-      <p>{props.data.definition}</p>
+      <span>({props.data.partOfSpeech})</span>
+      <span>{props.data.definition}</span>
     </div>
   );
 }
